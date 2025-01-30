c3d87f3b50bb31e3cfca79b50ef0434adb3d22b6
Debjit7212 <debjitpaul55555@gmail.com>
2025-01-30 16:30:10 +0530

    Your commit message

--- script.js
+++ script.js
@@ -9,30 +9,35 @@ let gif=document.getElementById('gif');
 let mastersongname =document.getElementById('mastersongname');
 let songItem= Array.from(document.getElementsByClassName('songItem'));
 let proformusic=[
-    {songname:"1 ",filepath:"0.mp3",coverpath:"sb.jpg"},
-    {songname:"2",filepath:"1.mp3",coverpath:"b.webp"},
-    {songname:"3",filepath:"2.mp3",coverpath:"c.webp"},
-    {songname:"4",filepath:"3.mp3",coverpath:"c.webp"},
-    {songname:"5",filepath:"4.mp3",coverpath:"sb.jpg"},
-    {songname:"6",filepath:"6.mp3",coverpath:"sb.jpg"},
-    {songname:"7",filepath:"7.mp3",coverpath:"sb.jpg"},
-    {songname:"8",filepath:"8.mp3",coverpath:"sb.jpg"},
-    {songname:"9",filepath:"9.mp3",coverpath:"sb.jpg"},
-    {songname:"10",filepath:"10.mp3",coverpath:"sb.jpg"},
-    {songname:"11",filepath:"11.mp3",coverpath:"sb.jpg"},
-    {songname:"12",filepath:"12.mp3",coverpath:"sb.jpg"},
-    {songname:"13",filepath:"13.mp3",coverpath:"sb.jpg"},
-    {songname:"14",filepath:"14.mp3",coverpath:"sb.jpg"},
-    {songname:"15",filepath:"15.mp3",coverpath:"sb.jpg"},
-    {songname:"16",filepath:"16.mp3",coverpath:"sb.jpg"}
-    
+    {songname:"romantic-song-tera-roothna-by-ashir- ",filepath:"0.mp3",coverpath:"sb.jpg"},
+    {songname:"kaise-bhula-dun-ashir-acoustic",filepath:"1.mp3",coverpath:"b.webp"},
+    {songname:"adhuri-kahani-jo-na-hosaki-puri",filepath:"2.mp3",coverpath:"c.webp"},
+    {songname:"romantic-",filepath:"3.mp3",coverpath:"c.webp"},
+    {songname:"tere-sang-with-you--",filepath:"4.mp3",coverpath:"sb.jpg"},
+    {songname:"Tujh-Mein-Rab-Dikhta-Hai-Remix",filepath:"5.mp3",coverpath:"sb.jpg"},
+    {songname:"Phir-Aur-Kya-Chahiye-Deep-House-Remix",filepath:"6.mp3",coverpath:"sb.jpg"},
+    {songname:"Bheega-Bheega-Hai-Sama-Remix",filepath:"7.mp3",coverpath:"sb.jpg"},
+    {songname:"Shree-Krishna-Mashup",filepath:"8.mp3",coverpath:"sb.jpg"},
+      {songname:"Rajar-Raja.",filepath:"9.mp3",coverpath:"sb.jpg"},
+    {songname:"Tera-Kasoor",filepath:"10.mp3",coverpath:"sb.jpg"},
+    {songname:"Hero-Honda.",filepath:"11.mp3",coverpath:"sb.jpg"},
+    {songname:"Dil-Tujhko-Chahe-Abhi-Dutt",filepath:"12.mp3",coverpath:"sb.jpg"},
+     {songname:"Peg-Marke",filepath:"13.mp3",coverpath:"sb.jpg"},
+    {songname:"Maadi",filepath:"14.mp3",coverpath:"sb.jpg"},
+    {songname:"Kishori-Antara-Mitra-Rathijit-Bhattacharjee.",filepath:"15.mp3",coverpath:"sb.jpg"},
+    {songname:"Kissik - Pushpa 2 The Rule ",filepath:"17.mp3",coverpath:"sb.jpg"},
+    {songname:"Bhool Bhulaiyaa 3 - Title Track (Feat. Pitbull)",filepath:"17.mp3",coverpath:"sb.jpg"}   
+   
 ]
+
 songItem.forEach((element,i)=>{
     console.log(element.i);
-   // element.getElementsByTagName("img")[0].src=proformusic[i].filepath;
+   
     element.getElementsByTagName("img")[0].src=proformusic[i].coverpath;
     element.getElementsByClassName("songname")[0].innerText=proformusic[i].songname;
+    
 })
+
 
 //audeoelement.play();
 //lision to event
@@ -77,7 +82,7 @@ element.addEventListener('click',(e)=>{
     songindex=parseInt(e.target.id);
     e.target.classList.remove('fa-circle-play');
     e.target.classList.add('fa-circle-pause');
-    audeoelement.src =(`${songindex+1}.mp3`);
+    audeoelement.src =(`${songindex}.mp3`);
     mastersongname.innerHTML=proformusic[songindex].songname;
     audeoelement.currentTime=0;
     audeoelement.play();
@@ -87,13 +92,13 @@ element.addEventListener('click',(e)=>{
 })
 })
 document.getElementById('next').addEventListener('click',()=>{
-    if(songindex>=15){
+    if(songindex>=proformusic.length){
         songindex=0
     }
     else{
         songindex +=1;
     }
-    audeoelement.src =(`${songindex+1}.mp3`);
+    audeoelement.src =(`${songindex}.mp3`);
     mastersongname.innerHTML=proformusic[songindex].songname;
     audeoelement.currentTime=0;
     audeoelement.play();
@@ -107,7 +112,7 @@ document.getElementById('previous').addEventListener('click',()=>{
     else{
         songindex -=1;
     }
-    audeoelement.src =(`${songindex+1}.mp3`);
+    audeoelement.src =(`${songindex}.mp3`);
     mastersongname.innerHTML=proformusic[songindex].songname;
     audeoelement.currentTime=0;
     audeoelement.play();
@@ -121,18 +126,27 @@ setInterval(function(){
     //clock.innerHTML=date.toLocaleDateString();
 },1000);
 
-function performSearch() {
-    const query = document.getElementById('search').value;
-    if (query) {
-       
-      alert(`You searched for: ${query}`);
-      // You can redirect to a search page or perform other actions here.
-      // Example: window.location.href = https://www.google.com/search?q=${query};
-    } else {
-      alert('Please enter a search term.');
+
+ // audeoelement.src = proformusic[songindex];
+  audeoelement.src =(`${songindex}.mp3`);
+  audeoelement.load();
+
+
+audeoelement.addEventListener("ended", () => {
+    
+    songindex++;
+    if (songindex >= proformusic.length) {
+        songindex = 0; 
     }
-  }
+
+    
+    audeoelement.src =(`${songindex}.mp3`);
+    mastersongname.innerHTML=proformusic[songindex].songname;
+    audeoelement.play();
+});
+//console.log(filteredproformusic.length);
+//audioPlayer.play();
+//console.log(proformusic.length);
 
 
-
-  
+    
